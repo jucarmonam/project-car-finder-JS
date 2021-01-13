75537deccaae8195b8160874a52ce18ed4a47b85
Juan <jucarmonam@unal.edu.co>
2021-01-13 12:24:14 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,166 @@
+const marca = document.querySelector("#marca");
+const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+const resultado = document.querySelector("#resultado");
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
+const datosBusqueda = {
+  marca: "",
+  year: "",
+  minimo: "",
+  maximo: "",
+  puertas: "",
+  transmision: "",
+  color: "",
+};
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarAutos(autos);
+
+  llenarSelect();
+});
+
+marca.addEventListener("change", (e) => {
+  datosBusqueda.marca = e.target.value;
+  filtrarAuto();
+});
+
+year.addEventListener("change", (e) => {
+  datosBusqueda.year = e.target.value;
+  filtrarAuto();
+});
+
+minimo.addEventListener("change", (e) => {
+  datosBusqueda.minimo = e.target.value;
+  filtrarAuto();
+});
+
+maximo.addEventListener("change", (e) => {
+  datosBusqueda.maximo = e.target.value;
+  filtrarAuto();
+});
+
+puertas.addEventListener("change", (e) => {
+  datosBusqueda.puertas = e.target.value;
+  filtrarAuto();
+});
+
+transmision.addEventListener("change", (e) => {
+  datosBusqueda.transmision = e.target.value;
+  filtrarAuto();
+});
+
+color.addEventListener("change", (e) => {
+  datosBusqueda.color = e.target.value;
+  filtrarAuto();
+  console.log(datosBusqueda);
+});
+
+function mostrarAutos(autos) {
+  limpiarHTML();
+  autos.forEach((auto) => {
+    const autoHTML = document.createElement("p");
+    const { marca, modelo, year, puertas, transmision, precio, color } = auto;
+
+    autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+        `;
+    //Insertamos en el html
+    resultado.appendChild(autoHTML);
+  });
+}
+
+function limpiarHTML() {
+  resultado.innerHTML = "";
+}
+
+function llenarSelect() {
+  for (let i = max; i >= min; i--) {
+    const opcion = document.createElement("option");
+    opcion.value = i;
+    opcion.textContent = i;
+    year.appendChild(opcion);
+  }
+}
+function filtrarAuto() {
+  const resultado = autos
+    .filter(filtrarMarca)
+    .filter(filtrarYear)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor);
+  console.log(resultado);
+
+  //mostrar resultados
+  if (resultado.length) {
+    mostrarAutos(resultado);
+  } else {
+    noResultado();
+  }
+}
+
+function noResultado() {
+  limpiarHTML();
+  const noResultado = document.createElement("div");
+  noResultado.classList.add("alerta", "error");
+  noResultado.textContent =
+    "No Hay Resultados, inteta con otros terminos de búsqueda";
+  resultado.appendChild(noResultado);
+}
+
+function filtrarMarca(auto) {
+  if (datosBusqueda.marca) {
+    return auto.marca === datosBusqueda.marca;
+  }
+  return auto;
+}
+
+function filtrarYear(auto) {
+  if (datosBusqueda.year) {
+    return auto.year === parseInt(datosBusqueda.year);
+  }
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  if (datosBusqueda.minimo) {
+    return auto.precio >= datosBusqueda.minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  if (datosBusqueda.maximo) {
+    return auto.precio <= datosBusqueda.maximo;
+  }
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  if (datosBusqueda.puertas) {
+    return auto.puertas == datosBusqueda.puertas;
+  }
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  if (datosBusqueda.transmision) {
+    return auto.transmision === datosBusqueda.transmision;
+  }
+  return auto;
+}
+
+function filtrarColor(auto) {
+  if (datosBusqueda.color) {
+    return auto.color === datosBusqueda.color;
+  }
+  return auto;
+}
